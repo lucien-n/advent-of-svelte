9ed39d65548ae7a121b52a7fcc2c7082028c328e
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-12-02 19:51:41 +0100

    moved santa

--- src/routes/two/+page.svelte
+++ src/routes/two/+page.svelte
@@ -4,20 +4,14 @@
 	import { Button } from '$shadcn/ui/button';
 	import { RotateCw } from 'lucide-svelte';
 	import { onMount } from 'svelte';
-	import { fly } from 'svelte/transition';
 	import CookiePopup from './cookie-popup.svelte';
 	import Cookie from './cookie.svelte';
+	import Santa from './santa.svelte';
 
 	let cookies = 0;
 	let parentNode: HTMLElement;
 	let cookiePopups: { id: number; comp: any; symbol: '+' | '-' }[] = [];
 	let rotateCookie = false;
-
-	const santaFaces = [
-		{ cookies: 500, img: 'santa-flabbergasted.jpg' },
-		{ cookies: 100, img: 'santa-awkward.jpg' },
-		{ cookies: 0, img: 'santa-happy.jpg' }
-	];
 
 	onMount(() => setCookies(getLocalCookies()));
 
@@ -58,23 +52,7 @@
 		<div
 			class="absolute w-24 aspect-square overflow-hidden -left-12 -top-12 rounded-full border-8 border-[#835cb6] bg-[#835cb6]"
 		>
-			{#each santaFaces as santaFace}
-				{#if cookies <= 0}
-					<img
-						src="santa-angry.jpg"
-						alt="santa angry"
-						class="rounded-full w-fit aspect-square object-cover"
-					/>
-				{:else if cookies >= santaFace.cookies}
-					<img
-						in:fly={{ x: -200, duration: 150, delay: 150 }}
-						out:fly={{ x: -200, duration: 150 }}
-						src={santaFace.img}
-						alt="santa"
-						class="rounded-full w-fit aspect-square object-cover"
-					/>
-				{/if}
-			{/each}
+			<Santa {cookies} />
 		</div>
 		<div class="mx-auto">
 			<Counter count={cookies} />
---
+++ src/routes/two/santa.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+
+	export let cookies: number;
+
+	const santaFaces = [
+		{ cookies: 500, img: 'santa-flabbergasted.jpg' },
+		{ cookies: 100, img: 'santa-awkward.jpg' },
+		{ cookies: 0, img: 'santa-happy.jpg' }
+	];
+</script>
+
+{#each santaFaces as santaFace}
+	{#if cookies <= 0}
+		<img
+			src="santa-angry.jpg"
+			alt="santa angry"
+			class="rounded-full w-fit aspect-square object-cover"
+		/>
+	{:else if cookies >= santaFace.cookies}
+		<img
+			in:fly={{ x: -200, duration: 150, delay: 150 }}
+			out:fly={{ x: -200, duration: 150 }}
+			src={santaFace.img}
+			alt="santa"
+			class="rounded-full w-fit aspect-square object-cover"
+		/>
+	{/if}
+{/each}

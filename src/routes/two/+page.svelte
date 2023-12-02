56c02bdf95980e96f77704ed3e8f60265a53a476
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-12-02 14:22:58 +0100

    cookie click animation

--- src/routes/two/+page.svelte
+++ src/routes/two/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { fly } from 'svelte/transition';
+	import CookiePopup from './cookie-popup.svelte';
 	import Cookie from './cookie.svelte';
 
 	let cookies = 0;
+	let parentNode: HTMLElement;
+	let cookiePopups: { id: number; comp: any }[] = [];
 
 	onMount(() => (cookies = getLocalCookies()));
 
@@ -12,15 +15,32 @@
 		else cookies -= 1;
 		if (cookies < 0) cookies = 0;
 		setLocalCookies(cookies);
+		createCookiePopup();
 	};
 
 	const setLocalCookies = (count: number) =>
 		localStorage.setItem('two:cookies', JSON.stringify(count));
 
 	const getLocalCookies = () => JSON.parse(localStorage.getItem('two:cookies') ?? '0');
+
+	const createCookiePopup = () => {
+		const id = Math.floor(Math.random() * 9999);
+		const comp = CookiePopup;
+		cookiePopups = [...cookiePopups, { id, comp }];
+		setTimeout(() => {
+			cookiePopups = cookiePopups.filter((popup) => popup.id !== id);
+		}, 1500);
+	};
 </script>
 
-<div class="flex flex-col items-center gap-8 justify-center h-full" in:fly={{ y: -200 }}>
-	<h1 class="text-5xl font-extrabold text-center">{cookies}</h1>
-	<Cookie on:click={handleCookieClick} />
+<div class="flex flex-col items-center justify-center h-full" in:fly={{ y: -200 }}>
+	<div class="flex flex-col relative gap-8" bind:this={parentNode}>
+		<h1 class="text-5xl font-extrabold text-center">{cookies}</h1>
+		<Cookie on:click={handleCookieClick} />
+		{#each cookiePopups as cookiePopup (cookiePopup.id)}
+			{#if parentNode}
+				<svelte:component this={cookiePopup.comp} {parentNode} />
+			{/if}
+		{/each}
+	</div>
 </div>
---
+++ src/routes/two/cookie-popup.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let parentNode: HTMLElement;
+
+	let node: HTMLSpanElement;
+	let x = 0;
+	let y = 0;
+
+	onMount(() => {
+		x = Math.floor(Math.random() * parentNode.clientWidth);
+		y = Math.floor(Math.random() * parentNode.clientHeight);
+
+		node.style.left = `${x}px`;
+		node.style.top = `${y}px`;
+	});
+</script>
+
+<span class="absolute cookie-popup pointer-events-none" bind:this={node}>
+	<p class="text-3xl">+1</p>
+</span>
+
+<style>
+	.cookie-popup {
+		animation: fly-fade-out 0.8s 0.05s forwards;
+	}
+
+	@keyframes fly-fade-out {
+		from {
+			opacity: 100%;
+		}
+		to {
+			opacity: 0%;
+			translate: 0px -150px;
+		}
+	}
+</style>
